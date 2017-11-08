4ffbddac5ebc1dec7dcddf582b66c172287d51df
JozefKamensky <jzfkamensky@gmail.com>
2017-11-08 14:25:05 +0100

    string literals and slices

--- 007_slices/src/main.rs
+++ 007_slices/src/main.rs
@@ -15,9 +15,21 @@ fn main() {
     // clear() will cause error, because of immutable reference via slice - see
     // references_and_borrowing - you cannot create mutable reference while at least one 
     // immutable reference exists 
-    s.clear();
+    //s.clear();
+
+    let literal = "string literal";
+    let literal_slice = find_first_word_both(&literal[..]);    
+    println!("1st word in literal \"{}\" is: {}", literal, literal_slice);
+
+    // string literals are slices, so we can omit slice syntax in function call
+    let literal_slice = find_first_word_both(&literal);
+    println!("1st word in literal \"{}\" is: {}", literal, literal_slice);
+
+    let string_slice = find_first_word_both(&s[..]);
+    println!("1st word in string \"{}\" is: {}", s, string_slice);
 }
 
+// function returning index of last + 1 character in first word
 fn find_first_word(string: &String) -> usize {
     let bytes = string.as_bytes();
 
@@ -30,7 +42,21 @@ fn find_first_word(string: &String) -> usize {
     string.len()
 }
 
+// function returning first word of String as slice
 fn find_first_word_slice(string: &String) -> &str {
+    let bytes = string.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &string[0..i];
+        }
+    }
+
+    &string[..]
+}
+
+// function returning first word of both String or string literal as slice - it is more generic
+fn find_first_word_both(string: &str) -> &str {
     let bytes = string.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate(){
